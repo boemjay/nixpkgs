f5368176461ede9cae843b18d66e174877c7d37b
Benjamin <benjamin.mollier@melli.com>
2022-10-15 18:06:38 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+# Experimental flake interface to Nixpkgs.
+# See https://github.com/NixOS/rfcs/pull/49 for details.
+{
+  description = "A collection of custom packages for the Nix package manager";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let out = system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          libcurl-example = import ./termdbms pkgs;
+        };
+      };
+    in flake-utils.lib.eachDefaultSystem out;
+}
